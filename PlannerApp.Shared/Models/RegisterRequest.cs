78923944bd0104525d581efb95cb66c6153f1939
Sean Hamilton <sean.hamilton@ieee.org>
2021-02-21 01:14:37 -0600

    Initial Commit

---
+++ PlannerApp.Shared/Models/RegisterRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlannerApp.Shared.Models {
+    public class RegisterRequest {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ConfirmPassword { get; set; }
+    }
+}
