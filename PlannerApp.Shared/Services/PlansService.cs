d236b603ea78d0a860c64ac8151b1200a3aaa39f
Sean Hamilton <sean.hamilton@ieee.org>
2021-03-16 23:37:40 -0500

    Added search to plans page

--- PlannerApp.Shared/Services/PlansService.cs
+++ PlannerApp.Shared/Services/PlansService.cs
@@ -38,7 +38,7 @@ namespace PlannerApp.Shared.Services {
         /// <param name="page"></param>
         /// <returns></returns>
         public async Task<PlansCollectionPagingResponse> SearchPlansByPageAsync(string query, int page = 1) {
-            var response = await client.GetProtectedAsync<PlansCollectionPagingResponse>($"{_baseUrl}/api/plans?query={query}&page={page}");
+            var response = await client.GetProtectedAsync<PlansCollectionPagingResponse>($"{_baseUrl}/api/plans/search?query={query}&page={page}");
             return response.Result;
         }
     }
